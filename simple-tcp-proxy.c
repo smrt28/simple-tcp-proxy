9b82ab063dd649b6b0f279bdcc726f2c97f9272c
Ondrej Holecek <ondrej.holecek@cdn77.com>
2023-06-27 04:03:13 -0400

    dev

--- simple-tcp-proxy.c
+++ simple-tcp-proxy.c
@@ -11,7 +11,7 @@
 #include <assert.h>
 #include <syslog.h>
 #include <err.h>
-
+#include <time.h>
 
 #include <sys/types.h>
 #include <sys/select.h>
@@ -24,14 +24,32 @@
 #include <sys/wait.h>
 
 #include <netinet/in.h>
-
+#include <inttypes.h>
 #include <arpa/ftp.h>
 #include <arpa/inet.h>
 #include <arpa/telnet.h>
+#include <poll.h>
 
 #define BUF_SIZE 4096
 
 char client_hostname[64];
+uint64_t start_time_;
+
+void sleep_double(double s) {
+    struct timespec t;
+    uint64_t ns = s * 1000000000;
+    t.tv_sec = ns / 1000000000;
+    t.tv_nsec = ns % 1000000000;
+    nanosleep(&t, NULL);
+}
+
+
+uint64_t now() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000000 + tv.tv_usec) - start_time_;
+}
+
 
 void
 cleanup(int sig)
@@ -193,7 +211,7 @@ service_client(int cfd, int sfd)
     int tmp;
     int logcnt = 0;
     int ctotal = 0;
-    int stotal = 0; 
+    int stotal = 0;
 
     printf("service client started\n");
 
@@ -203,65 +221,104 @@ service_client(int cfd, int sfd)
     maxfd++;
 
     while (1) {
-	struct timeval to;
-	if (cbo) {
-        tmp = mywrite(sfd, cbuf, &cbo);
+        struct timeval to;
+        if (cbo) {
+            tmp = mywrite(sfd, cbuf, &cbo);
 
-		if (tmp < 0 && errno != EWOULDBLOCK) {
-            printf("ERROR: write (cbo) failed; errno=%d", errno);
-            exit(1);
-		}
-        ctotal += tmp;
-	}
-	if (sbo) {
-        tmp = mywrite(cfd, sbuf, &sbo);
-		if (tmp < 0 && errno != EWOULDBLOCK) {
-            printf("ERROR: write (sbo) failed; errno=%d", errno);
-            exit(1);
-		}
-        stotal += tmp;
-	}
+            if (tmp < 0 && errno != EWOULDBLOCK) {
+                printf("ERROR: write (cbo) failed; errno=%d", errno);
+                exit(1);
+            }
+            ctotal += tmp;
+        }
+        if (sbo) {
+            tmp = mywrite(cfd, sbuf, &sbo);
+            if (tmp < 0 && errno != EWOULDBLOCK) {
+                printf("ERROR: write (sbo) failed; errno=%d", errno);
+                exit(1);
+            }
+            stotal += tmp;
+        }
 
-    logcnt++;
-    if (logcnt % 5000 == 0) {
-        printf("%d %d %d\n", logcnt, ctotal, stotal);
+        logcnt++;
+        if (logcnt % 5000 == 0) {
+            printf("%d %d %d\n", logcnt, ctotal, stotal);
+        }
+
+        FD_ZERO(&R);
+        if (cbo < BUF_SIZE)
+            FD_SET(cfd, &R);
+        if (sbo < BUF_SIZE)
+            FD_SET(sfd, &R);
+        to.tv_sec = 0;
+        to.tv_usec = 1000;
+        x = select(maxfd+1, &R, 0, 0, &to);
+        if (x > 0) {
+            if (FD_ISSET(cfd, &R)) {
+                n = read(cfd, cbuf+cbo, BUF_SIZE-cbo);
+                syslog(LOG_INFO, "read %d bytes from CLIENT (%d)", n, cfd);
+                if (n > 0) {
+                    cbo += n;
+                } else {
+                    close(cfd);
+                    close(sfd);
+                    _exit(0);
+                }
+            }
+            if (FD_ISSET(sfd, &R)) {
+                n = read(sfd, sbuf+sbo, BUF_SIZE-sbo);
+                if (n > 0) {
+                    sbo += n;
+                } else {
+                    close(sfd);
+                    close(cfd);
+                    _exit(0);
+                }
+            }
+        } else if (x < 0 && errno != EINTR) {
+            close(sfd);
+            close(cfd);
+            _exit(0);
+        }
     }
+}
 
-	FD_ZERO(&R);
-	if (cbo < BUF_SIZE)
-		FD_SET(cfd, &R);
-	if (sbo < BUF_SIZE)
-		FD_SET(sfd, &R);
-	to.tv_sec = 0;
-	to.tv_usec = 1000;
-	x = select(maxfd+1, &R, 0, 0, &to);
-	if (x > 0) {
-	    if (FD_ISSET(cfd, &R)) {
-		n = read(cfd, cbuf+cbo, BUF_SIZE-cbo);
-		syslog(LOG_INFO, "read %d bytes from CLIENT (%d)", n, cfd);
-		if (n > 0) {
-		    cbo += n;
-		} else {
-		    close(cfd);
-		    close(sfd);
-		    _exit(0);
-		}
-	    }
-	    if (FD_ISSET(sfd, &R)) {
-		n = read(sfd, sbuf+sbo, BUF_SIZE-sbo);
-		if (n > 0) {
-		    sbo += n;
-		} else {
-		    close(sfd);
-		    close(cfd);
-		    _exit(0);
-		}
-	    }
-	} else if (x < 0 && errno != EINTR) {
-	    close(sfd);
-	    close(cfd);
-	    _exit(0);
-	}
+void idle_client(int fd) {
+    struct pollfd pfd;
+    char buf[1024];
+    int len = 0;
+
+    fprintf(stderr, "%" PRId64 " idle_client started\n", now());
+
+    for (;;) {
+        pfd.fd = fd;
+        pfd.events = POLLIN;
+        int ready = poll(&pfd, 1, -1);
+        if (ready == -1) {
+            fprintf(stderr, "err: poll\n");
+            return;
+        }
+
+        for (;;) {
+            int res = read(fd, buf, sizeof(buf));
+            if (res < 0) {
+                fprintf(stderr, "err: read\n");
+                return;
+            }
+
+            if (res == 0) {
+                fprintf(stderr, "%" PRId64 " connection closed\n", now());
+                return;
+            }
+
+            len += res;
+
+            if (res < sizeof(buf)) {
+                break;
+            }
+
+            fprintf(stderr, "read: %d\n", len);
+        }
     }
 }
 
@@ -276,21 +333,34 @@ main(int argc, char *argv[])
     int client = -1;
     int server = -1;
     int master_sock = -1;
+    int delay = 0;
+    int odd = 1;
+    int cnt = 0;
 
-    if (5 != argc) {
-	fprintf(stderr, "usage: %s laddr lport rhost rport\n", argv[0]);
-	exit(1);
+    if (argc < 5) {
+        fprintf(stderr, "usage: %s laddr lport rhost rport\n", argv[0]);
+        exit(1);
     }
+
+    start_time_ = now();
 
     localaddr = strdup(argv[1]);
     localport = atoi(argv[2]);
     remoteaddr = strdup(argv[3]);
     remoteport = atoi(argv[4]);
+    if (argc >= 6) {
+        delay = atoi(argv[5]);
+    }
 
     assert(localaddr);
     assert(localport > 0);
     assert(remoteaddr);
     assert(remoteport > 0);
+
+
+    fprintf(stderr, "tunneling: %s:%d <=> %s:%d\n", localaddr, localport, remoteaddr, remoteport);
+    fprintf(stderr, "delay: %d\n", delay);
+    fprintf(stderr, "=================================================================\n");
 
     openlog(argv[0], LOG_PID, LOG_LOCAL4);
 
@@ -298,11 +368,19 @@ main(int argc, char *argv[])
     signal(SIGCHLD, sigreap);
 
     master_sock = create_server_sock(localaddr, localport);
-    for (;;) {
-        printf("loop next...\n");
+    for (;;++cnt) {
+        printf("[%d] waiting for connection...\n", cnt);
         if ((client = wait_for_connection(master_sock)) < 0)
             continue;
-        printf("connect...\n");
+        if (remoteaddr[0] == 'x' && remoteaddr[1] == 0) {
+            printf("%" PRId64 " emulating timeout\n", now());
+            if (0 == fork()) {
+                idle_client(client);
+                return 0;
+            }
+            continue;
+        }
+        printf("connect > %s\n", remoteaddr);
         if ((server = open_remote_host(remoteaddr, remoteport)) < 0) {
             printf("ERROR: open remote host failed....\n");
             close(client);
@@ -314,10 +392,19 @@ main(int argc, char *argv[])
             printf("in fork\n");
             /* child */
             close(master_sock);
+            if (delay) {
+                if (odd) {
+                    printf("sleeping: %dms\n", delay);
+                    sleep_double(((double)delay) / 1000);
+                } else {
+                    printf("sleeping: %dms - skip\n", delay);
+                }
+            }
             service_client(client, server);
             printf("clent exited...\n");
             abort();
         }
+        odd ^= 1;
         close(client);
         client = -1;
         close(server);
